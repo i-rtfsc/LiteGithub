3f39cb5935fd70b2f14b1b899f7ab62b0a4a1e9b
Solo <anqi.huang@outlook.com>
2018-05-20 12:25:26 +0800

    Remove mini drawer
    1. Sometime click event don't work for mini drawer
    2. Maybe we will use this drawer on another activity
    3. Maybe we will use another drawer library on the future（This drawwe library has lots of issues）
    
    Signed-off-by: Solo <anqi.huang@outlook.com>

--- github/src/com/journeyOS/github/ui/main/GithubActivity.java
+++ github/src/com/journeyOS/github/ui/main/GithubActivity.java
@@ -18,16 +18,11 @@ package com.journeyOS.github.ui.main;
 
 import android.arch.lifecycle.Observer;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.GravityCompat;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 
 import com.journeyOS.base.Constant;
 import com.journeyOS.base.utils.LogUtils;
@@ -36,28 +31,15 @@ import com.journeyOS.core.CoreManager;
 import com.journeyOS.core.api.userprovider.AuthUser;
 import com.journeyOS.core.base.BaseActivity;
 import com.journeyOS.core.viewmodel.ModelProvider;
+import com.journeyOS.github.MaterialDrawer;
+import com.journeyOS.github.MaterialDrawer.OnDrawerItemClickListener;
 import com.journeyOS.github.R;
 import com.journeyOS.github.ui.fragment.repos.ReposFragment;
-import com.mikepenz.crossfadedrawerlayout.view.CrossfadeDrawerLayout;
-import com.mikepenz.materialdrawer.AccountHeader;
-import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.MiniDrawer;
-import com.mikepenz.materialdrawer.interfaces.ICrossfader;
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
-import com.mikepenz.materialdrawer.model.ProfileSettingDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
-import com.mikepenz.materialdrawer.model.SectionDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IProfile;
-import com.mikepenz.materialdrawer.util.DrawerUIUtils;
-import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 
-public class GithubActivity extends BaseActivity {
+public class GithubActivity extends BaseActivity implements OnDrawerItemClickListener {
     private static final String TAG = GithubActivity.class.getSimpleName();
 
     @BindView(R.id.toolbar)
@@ -68,9 +50,8 @@ public class GithubActivity extends BaseActivity {
     GithubModel mGithubModel;
     Context mContext;
 
-    private AccountHeader headerResult = null;
+    private MaterialDrawer mMaterialDrawer;
     private Drawer result = null;
-    private CrossfadeDrawerLayout crossfadeDrawerLayout = null;
 
     @Override
     public void initBeforeView() {
@@ -87,6 +68,8 @@ public class GithubActivity extends BaseActivity {
     public void initViews() {
         UIUtils.setStatusBarColor(this, this.getResources().getColor(R.color.colorPrimary));
         setSupportActionBar(mToolbar);
+        mMaterialDrawer = new MaterialDrawer(this);
+        mMaterialDrawer.setOnDrawerItemClickListener(this);
     }
 
 
@@ -100,7 +83,7 @@ public class GithubActivity extends BaseActivity {
             @Override
             public void onChanged(@Nullable final AuthUser user) {
                 CoreManager.setAccessToken(user.accessToken);
-                initDrawer(bundle, user);
+                result = mMaterialDrawer.initDrawer(bundle, user, mToolbar);
                 loadFragment(ReposFragment.newInstance(ReposFragment.ReposType.OWNED));
             }
         });
@@ -111,7 +94,7 @@ public class GithubActivity extends BaseActivity {
         //add the values which need to be saved from the drawer to the bundle
         outState = result.saveInstanceState(outState);
         //add the values which need to be saved from the accountHeader to the bundle
-        outState = headerResult.saveInstanceState(outState);
+//        outState = headerResult.saveInstanceState(outState);
         super.onSaveInstanceState(outState);
     }
 
@@ -125,151 +108,49 @@ public class GithubActivity extends BaseActivity {
         }
     }
 
-    void initDrawer(Bundle savedInstanceState, AuthUser user) {
-
-        ImageView imageView = new ImageView(mContext);
-        Picasso.with(mContext)
-                .load(Uri.parse(user.avatar))
-                .placeholder(R.mipmap.user)
-                .into(imageView);
-
-        final IProfile profile = new ProfileDrawerItem()
-                .withName(user.name)
-                .withEmail(user.email)
-                .withIcon(imageView.getDrawable());
-
-        headerResult = new AccountHeaderBuilder()
-                .withActivity(this)
-                .withCompactStyle(true)
-                .addProfiles(profile,
-                        new ProfileSettingDrawerItem().withName(R.string.add_account).withDescription(R.string.add_new_account).withIcon(R.drawable.svg_add_user).withIdentifier(9),
-                        new ProfileSettingDrawerItem().withName(R.string.manage_account).withIcon(R.drawable.svg_menu_settings))
-                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
-                    @Override
-                    public boolean onProfileChanged(View view, IProfile profile, boolean current) {
-                        LogUtils.d(TAG, "onProfileChanged() called with: view = [" + view + "], profile = [" + profile + "], current = [" + current + "]");
-                        return false;
-                    }
-                })
-                .withOnAccountHeaderSelectionViewClickListener(new AccountHeader.OnAccountHeaderSelectionViewClickListener() {
-                    @Override
-                    public boolean onClick(View view, IProfile profile) {
-                        LogUtils.d(TAG, "onClick() called with: view = [" + view + "], profile = [" + profile + "]");
-                        return false;
-                    }
-                })
-                .build();
-
-        result = new DrawerBuilder()
-                .withActivity(this)
-                .withToolbar(mToolbar)
-                .withHasStableIds(true)
-                .withDrawerLayout(R.layout.crossfade_drawer)
-                .withDrawerWidthDp(72)
-                .withActionBarDrawerToggleAnimated(true)
-                .withGenerateMiniDrawer(true)
-                .withAccountHeader(headerResult)
-//                .withRootView(R.id.drawer_container)
-                .withDisplayBelowStatusBar(false)
-                .addDrawerItems(
-                        new PrimaryDrawerItem().withIdentifier(1).withName(R.string.profile).withIcon(R.drawable.svg_menu_user).withTag(Constant.MENU_PROFILE),
-                        new PrimaryDrawerItem().withIdentifier(2).withName(R.string.my_repos).withIcon(R.drawable.svg_menu_repos).withTag(Constant.MENU_REPOS),
-                        new PrimaryDrawerItem().withIdentifier(3).withName(R.string.notifications).withIcon(R.drawable.svg_menu_notification).withTag(Constant.MENU_NOTIFICATION),
-                        new PrimaryDrawerItem().withIdentifier(4).withName(R.string.issues).withIcon(R.drawable.svg_menu_issue).withTag(Constant.MENU_ISSUE),
-                        new SectionDrawerItem().withName(R.string.advanced),
-                        new SecondaryDrawerItem().withIdentifier(5).withName(R.string.search).withIcon(R.drawable.svg_menu_search).withTag(Constant.MENU_SEARCH),
-                        new SecondaryDrawerItem().withIdentifier(6).withName(R.string.starred).withIcon(R.drawable.svg_menu_starred).withTag(Constant.MENU_STARRED)
-                )
-                .addStickyDrawerItems(
-                        new SecondaryDrawerItem().withIdentifier(7).withName(R.string.settings).withIcon(R.drawable.svg_menu_settings).withTag(Constant.MENU_SETTINGS),
-                        new SecondaryDrawerItem().withIdentifier(8).withName(R.string.about).withIcon(R.drawable.svg_menu_about).withTag(Constant.MENU_ABOUT)
-                )
-                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                    @Override
-                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        if (result != null && result.isDrawerOpen()) {
-                            result.closeDrawer();
-                        }
-                        String tag = (String) drawerItem.getTag();
-                        handleDrawerItem(tag, false);
-                        return false;
-                    }
-                })
-                .withOnDrawerItemLongClickListener(new Drawer.OnDrawerItemLongClickListener() {
-                    @Override
-                    public boolean onItemLongClick(View view, int position, IDrawerItem drawerItem) {
-                        if (result != null && result.isDrawerOpen()) {
-                            result.closeDrawer();
-                        }
-                        String tag = (String) drawerItem.getTag();
-                        handleDrawerItem(tag, true);
-                        return false;
-                    }
-                })
-                .withFireOnInitialOnClick(true)
-                .withSavedInstance(savedInstanceState)
-                .build();
-
-        crossfadeDrawerLayout = (CrossfadeDrawerLayout) result.getDrawerLayout();
-        crossfadeDrawerLayout.setMaxWidthPx(DrawerUIUtils.getOptimalDrawerWidth(mContext));
-        final MiniDrawer miniResult = result.getMiniDrawer();
-        View view = miniResult.build(mContext);
-        view.setBackgroundColor(mContext.getResources().getColor(R.color.darkslategray));
-        crossfadeDrawerLayout.getSmallView().addView(view, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        miniResult.withCrossFader(new ICrossfader() {
-            @Override
-            public void crossfade() {
-                boolean isFaded = isCrossfaded();
-                crossfadeDrawerLayout.crossfade(400);
-
-                if (isFaded) {
-                    result.getDrawerLayout().closeDrawer(GravityCompat.START);
-                }
-            }
-
-            @Override
-            public boolean isCrossfaded() {
-                return crossfadeDrawerLayout.isCrossfaded();
-            }
-        });
-
-        mFrameLayoutContent.setBackgroundColor(mContext.getResources().getColor(R.color.main_background));
-    }
-
-    void handleDrawerItem(String tag, boolean longClick) {
-        LogUtils.d(TAG, "handle drawer item = " + tag + " , is long click = " + longClick);
-        switch (tag) {
-            case Constant.MENU_PROFILE:
-                showShortToast(ToastType.INFO, mContext.getString(R.string.developing));
-                break;
-            case Constant.MENU_REPOS:
-                loadFragment(ReposFragment.newInstance(ReposFragment.ReposType.OWNED));
-                break;
-            case Constant.MENU_NOTIFICATION:
-                showShortToast(ToastType.INFO, mContext.getString(R.string.developing));
-                break;
-            case Constant.MENU_ISSUE:
-                showShortToast(ToastType.INFO, mContext.getString(R.string.developing));
-                break;
-            case Constant.MENU_SEARCH:
-                showShortToast(ToastType.INFO, mContext.getString(R.string.developing));
-                break;
-            case Constant.MENU_STARRED:
-                loadFragment(ReposFragment.newInstance(ReposFragment.ReposType.STARRED));
-                break;
-            case Constant.MENU_SETTINGS:
-                showShortToast(ToastType.INFO, mContext.getString(R.string.developing));
-                break;
-            case Constant.MENU_ABOUT:
-                showShortToast(ToastType.INFO, mContext.getString(R.string.developing));
-                break;
-        }
-    }
-
     void loadFragment(Fragment fragment) {
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.frame_container, fragment)
                 .commit();
+    }
+
+    @Override
+    public void handleDrawerItem(String tag, boolean longClick) {
+        LogUtils.d(TAG, "handle drawer item = " + tag + " , is long click = " + longClick);
+        switch (tag) {
+            case Constant.MENU_PROFILE:
+                mToolbar.setTitle(R.string.profile);
+                showShortToast(ToastType.INFO, mContext.getString(R.string.developing));
+                break;
+            case Constant.MENU_REPOS:
+                mToolbar.setTitle(R.string.my_repos);
+                loadFragment(ReposFragment.newInstance(ReposFragment.ReposType.OWNED));
+                break;
+            case Constant.MENU_NOTIFICATION:
+                mToolbar.setTitle(R.string.notifications);
+                showShortToast(ToastType.INFO, mContext.getString(R.string.developing));
+                break;
+            case Constant.MENU_ISSUE:
+                mToolbar.setTitle(R.string.issues);
+                showShortToast(ToastType.INFO, mContext.getString(R.string.developing));
+                break;
+            case Constant.MENU_SEARCH:
+                mToolbar.setTitle(R.string.search);
+                showShortToast(ToastType.INFO, mContext.getString(R.string.developing));
+                break;
+            case Constant.MENU_STARRED:
+                mToolbar.setTitle(R.string.starred);
+                loadFragment(ReposFragment.newInstance(ReposFragment.ReposType.STARRED));
+                break;
+            case Constant.MENU_SETTINGS:
+                mToolbar.setTitle(R.string.settings);
+                showShortToast(ToastType.INFO, mContext.getString(R.string.developing));
+                break;
+            case Constant.MENU_ABOUT:
+                mToolbar.setTitle(R.string.about);
+                showShortToast(ToastType.INFO, mContext.getString(R.string.developing));
+                break;
+        }
     }
 }
